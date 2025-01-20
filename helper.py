86015d6571f63e75d3218bbf4f29a6baffb0acff
Karina Basto Eyzaguirre <karieyz.25@gmail.com>
2025-01-20 14:01:32 -0500

    Adding description to the notebook & updating feature selection

--- helper.py
+++ helper.py
@@ -51,3 +51,45 @@ def compare_models_with_dummies(df, target_col, feature_cols, test_size=0.2, ran
     }
 
   return results
+
+
+def compare_models_with_numerical_features(df, target_col, feature_cols, test_size=0.2, random_state=42):
+  """
+  Function to compare the fit of different numerical features to explain the target variable. The function was built to take only numerical features.
+  
+  Args:
+    df: Dataframe containing the data
+    target_col: Name of the target column.
+    feature_cols: List of numerical feature column names to compare.
+    test_size: Proportion of data to use for testing, in this case I am using 20%.
+    random_state: Seed for random number generator for reproducibility.
+
+  Returns:
+    A dictionary where keys are feature names and values are dictionaries containing 
+    model summary statistics (e.g., R-squared, adjusted R-squared, AIC, BIC).
+  """
+
+  results = {}
+  for feature in feature_cols:
+    # Select features and target
+    X = df[[feature]]
+    y = df[target_col]
+
+    # Split data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+
+    # Add constant to the training data
+    X_train = sm.add_constant(X_train) 
+
+    # Fit the model on training data
+    model = sm.OLS(y_train, X_train).fit()
+
+    # Calculate metrics on training data
+    results[feature] = {
+        'R-squared': model.rsquared,
+        'Adjusted R-squared': model.rsquared_adj,
+        'AIC': model.aic,
+        'BIC': model.bic
+    }
+
+  return results
